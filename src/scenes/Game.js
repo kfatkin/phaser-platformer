c53d6e67fd914877545308a4531e733df57fc3da
Kevin Fatkin <kfatkin@gmail.com>
2021-03-30 00:51:04 -0400

    first commit

---
+++ src/scenes/Game.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+
+class Game extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GameScene' });
+  }
+
+  preload() {
+    this.load.image('logo', 'assets/phaser3-logo.png');
+  }
+
+  create(data) {
+    this.add.image(400, 300, 'logo');
+  }
+
+  update(time, delta) {}
+}
+
+export default Game;
